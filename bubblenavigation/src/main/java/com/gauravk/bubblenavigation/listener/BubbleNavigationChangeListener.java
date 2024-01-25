41eaa7345d5349936613e18dc7b2b2f1c9fb5793
Dong0610 <bdong0610@gmail.com>
2024-01-25 22:08:10 +0700

    Add Bottom navigation to App

---
+++ bubblenavigation/src/main/java/com/gauravk/bubblenavigation/listener/BubbleNavigationChangeListener.java
@@ -0,0 +1,8 @@
+
+package com.gauravk.bubblenavigation.listener;
+
+import android.view.View;
+
+public interface BubbleNavigationChangeListener {
+    void onNavigationChanged(View view, int position);
+}
---
+++ bubblenavigation/src/main/java/com/gauravk/bubblenavigation/util/ViewUtils.java
@@ -0,0 +1,28 @@
+
+package com.gauravk.bubblenavigation.util;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.TypedValue;
+
+import org.jetbrains.annotations.Nullable;
+
+public class ViewUtils {
+
+    public static int getThemeAccentColor(final Context context) {
+        final TypedValue value = new TypedValue();
+        context.getTheme().resolveAttribute(androidx.constraintlayout.widget.R.attr.colorAccent, value, true);
+        return value.data;
+    }
+
+    public static void updateDrawableColor(@Nullable Drawable drawable, int color) {
+        if (drawable == null) return;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            drawable.setTint(color);
+        else
+            drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+    }
+
+}
